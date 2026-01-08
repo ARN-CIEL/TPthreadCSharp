25c94b5e3694f3b254ba0c33e311c357fc7bd1c2
Nathan Arnould <nathan.arnould.pro@gmail.com>
2026-01-08 10:24:59 +0100

    #3

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿class Program
+﻿using CTest;
+class Program
 {
     static void AfficheA()
     {
@@ -17,16 +18,23 @@
         Thread.CurrentThread.Name = "Thread 1";
         Console.WriteLine(Thread.CurrentThread.Name);
 
-        Thread thread1_fonction = new Thread(AfficheA);
-        Thread thread2_fonction = new Thread(AfficheA);
-        Thread thread3_fonction = new Thread(AfficheA);
+        CTest.CTest test = new CTest.CTest();
+        Thread thread1_fonction1 = new Thread(AfficheA);
+        Thread thread2_fonction1 = new Thread(AfficheA);
+        Thread thread3_fonction1 = new Thread(AfficheA);
+        Thread thread4_fonction2 = new Thread(test.AfficheB);
+        Thread thread5_fonction3 = new Thread(test.AfficheC);
 
-        thread1_fonction.Name = "thread1_fonction1";
-        thread2_fonction.Name = "thread2_fonction1";
-        thread3_fonction.Name = "thread3_fonction1";
+        thread1_fonction1.Name = "thread1_fonction1";
+        thread2_fonction1.Name = "thread2_fonction1";
+        thread3_fonction1.Name = "thread3_fonction1";
+        thread4_fonction2.Name = "thread4_fonction2";
+        thread5_fonction3.Name = "thread5_fonction3";
 
-        thread1_fonction.Start();
-        thread2_fonction.Start();
-        thread3_fonction.Start();
+        //thread1_fonction.Start();
+        //thread2_fonction.Start();
+        //thread3_fonction.Start();
+        thread4_fonction2.Start();
+        thread5_fonction3.Start();
     }
 }
